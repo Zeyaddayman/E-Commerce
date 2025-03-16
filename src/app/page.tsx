90bdfe7615307ef0c56c57223906b6def28efb8e
Zeyad_Ayman <zezoayman0100@yahoo.com>
2025-03-16 16:31:32 +0200

    enhance route system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
-import { redirect } from "next/navigation";
+import { PrismaClient } from "@prisma/client";
+import Link from "next/link";
+
+const prisma = new PrismaClient()
 
 export default async function Home() {
-    redirect("/products");
+    const products = await prisma.product.findMany({
+        select: {
+            category: true
+        }
+    })
+
+    const productsCategories: { [category: string]: number } = {}
+
+    for (let i = 0; i < products.length; i++) {
+        productsCategories[products[i].category] = productsCategories[products[i].category] + 1 || 1
+    }
+
+    return (
+        <>
+        {Object.keys(productsCategories).map((category) => (
+            <Link key={category} href={category} className="block">
+                {category} : {productsCategories[category]}
+            </Link>
+        ))}
+        </>
+    )
 }
